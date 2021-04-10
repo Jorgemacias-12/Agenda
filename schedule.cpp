c8bee900d89b829398c0075ee377ecbfb5ee315e
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-04-10 12:09:47 -0500

    Comprobar si se esta ejecutando windows o linux

--- schedule.cpp
+++ schedule.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
-
+    #ifdef __linux__
+        cout << "Estoy en linux :u" << endl;
+    #endif
+    #ifdef __WIN32
+        cout << "Estoy en windows :u" << endl;
+    #endif
 }
