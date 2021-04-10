258b803a4e4f0d406bf7bc68f70d80fdea65befb
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-04-10 15:27:34 -0500

    Funcion principal y menu añadidos ademas de funciones de utilidad

--- schedule.cpp
+++ schedule.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
+#include <locale.h>
+
 using namespace std;
+
+void initLocale()
+{
+    setlocale(LC_ALL, "");
+}
+
+void clearScreen(int os)
+{
+    switch (os)
+    {
+        case 1:
+            system("clear");
+            break;
+        case 2:
+            system("cls");
+            break;
+    }
+}
+
+void appMenu()
+{
+i:
+    int menuOption = 0;
+    cout << "---Menú---" << endl;
+    cout << "1. Ingresar contactos." << endl;
+    cout << "2. Mostrar  contactos." << endl;
+    cout << "3. Salir." << endl;
+    cin >> menuOption;
+    switch (menuOption)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        cout << "Saliendo..." << endl;
+        break;
+    default:
+        #ifdef __linux__
+            clearScreen(1);
+        #elif _WIN32
+            clearScreen(2);
+        #endif
+        goto i;
+        break;
+    }
+}
 
 int main()
 {
-    #ifdef __linux__
-        cout << "Estoy en linux :u" << endl;
-    #endif
-    #ifdef __WIN32
-        cout << "Estoy en windows :u" << endl;
-    #endif
+    initLocale();
+    appMenu();
 }
